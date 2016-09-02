f24eaf772cf7f72a6553bc891b51398d228f4a74
Etsitra <wadowskil@gmail.com>
2016-09-02 12:14:52 +0100

    Add simple grunt setup

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Unknown on 2016-09-02.
+ */
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.fileName %>.js',
+                dest: 'build/<%= pkg.fileName %>.min.js'
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+    grunt.registerTask('build', ['uglify']);
+
+};
